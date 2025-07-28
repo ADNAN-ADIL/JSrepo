5fb3f03c3291c60ffc999c4e6721654e6d01aabf
ADNAN-ADIL <mohammedadnanadil@gmail.com>
2025-07-28 22:38:18 +0530

    events and form validations

--- JS events/avg.js
+++ JS events/avg.js
@@ -1,4 +1,5 @@
-function doAvg(){
+
+function doAvg() {
     let a = document.getElementById('t1').value
     let b = document.getElementById('t2').value
     let c = document.getElementById('t3').value
@@ -6,11 +7,11 @@ function doAvg(){
     let e = document.getElementById('t5').value
     let f = document.getElementById('t6').value
 
-    let sum= eval(a)+eval(b)+eval(c)+eval(d)+eval(e)+eval(f)
-    let avg = sum/6
+    let sum = eval(a) + eval(b) + eval(c) + eval(d) + eval(e) + eval(f)
+    let avg = sum / 6
     alert(avg)
 }
-function eraseData(){
+function eraseData() {
     document.getElementById('t1').value = ""
     document.getElementById('t2').value = ""
     document.getElementById('t3').value = ""
